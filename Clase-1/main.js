4f83c83815e9c408ce482f301e788d6ccc6cc9d5
NachoBasilio <ignacio.n.basilio.b@gmail.com>
2024-11-09 13:28:43 -0300

    Primer clase

---
+++ Clase-1/main.js
@@ -0,0 +1,57 @@
+// console.log("Hola")
+
+// let nombre = "Nacho\nBasilio"
+
+// console.log(nombre)
+
+// let edad = 16
+
+// let esMayor = edad >= 18
+
+// console.log(esMayor)
+
+// let casado = false
+
+// let nombreDeGato
+
+
+// nombreDeGato = "Rengar"
+
+
+// console.log(nombreDeGato)
+
+
+// const PI = 3.1416
+// const URL = "www.google.com"
+
+// console.log(edad)
+
+// edad = "Nacho"
+
+// console.log(edad)
+
+// console.log(1 == "1")
+// console.log(1 === "1")
+
+// let nombre = prompt("Como te llamas?")
+
+// alert("Hola, " + nombre + " ¿Como estas?")
+
+// let confirmacion = confirm("¿Quiere seguir navegando?")
+
+// let edad = Number(prompt("¿Cuantos años tenes?"))
+
+// console.log(isNaN(edad))
+
+// console.log(edad + 20)
+// console.log(13)
+
+let peso = Number(prompt("Introduce tu peso en kilogramos: "))
+
+
+let altura = Number(prompt("Introduce tu altura en metros: "))
+
+
+let imc = peso/(altura * altura)
+
+alert("Tu indice de masa corporal es de: " + imc.toFixed(2))
